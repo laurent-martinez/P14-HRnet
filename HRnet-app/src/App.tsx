510211c07579010849c8f0056cab95102e9f9a7b
laurent-martinez <martilaurier@gmail.com>
2022-11-23 13:57:38 +0100

    add lazy load to employees page

--- HRnet-app/src/App.tsx
+++ HRnet-app/src/App.tsx
@@ -1,17 +1,21 @@
+import React, { lazy, Suspense } from "react";
 import "./fonts/Ubuntu-Medium.ttf";
 import "./fonts/SourceSansPro-Bold.ttf";
 import "./styles/index.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Employees from "./pages/Employees";
 import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
+
+const Employees = lazy(() => import("./pages/Employees"));
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Register />} />
-        <Route path="/Employees" element={<Employees />} />
+        <Suspense fallback={<h1>Loading ...</h1>}>
+          <Route path="/Employees" element={<Employees />} />
+        </Suspense>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
